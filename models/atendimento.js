97cb97fcff073542d7e0c59cfbca5d972545431b
alvaro alves <alvaroalves87@gmail.com>
2020-09-05 22:20:01 -0300

    Refining atendimento.js

--- models/atendimento.js
+++ models/atendimento.js
@@ -1,10 +1,15 @@
 const conexao = require('../infra/connection')
+const moment = require('moment')
 
 class Atendimento{
     adiciona(atendimento){
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+
+        const atendimentoDatado = {...atendimento, dataCriacao, data}
         const sql = 'INSERT INTO atendimentos SET ?'
 
-        conexao.query(sql, atendimento, (err, results) => {
+        conexao.query(sql, atendimentoDatado, (err, results) => {
             if(err){
                 console.log(err)
             }else{
